fa255ef95e3bb5c9e517cb1905e334ae1154f112
Rylanristia <50695667+rylanristia@users.noreply.github.com>
2022-03-06 19:03:50 +0700

    Splashcreen change runtime

--- resources/views/layouts/splashscreen.blade.php
+++ resources/views/layouts/splashscreen.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="15;url={{ route('welcome') }}" />
+    <meta http-equiv="refresh" content="20;url={{ route('welcome') }}" />
     <title>Hi Aprilia</title>
 
     <link rel="stylesheet" href="./styles/splashscreen.css">
