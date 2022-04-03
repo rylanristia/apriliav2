2b0bbf86222a0f7be9d9a08eb908c322290b56f7
Rylanristia <50695667+rylanristia@users.noreply.github.com>
2022-04-03 16:19:23 +0700

    from-me update

--- resources/views/pages/rylanristia.blade.php
+++ resources/views/pages/rylanristia.blade.php
@@ -12,7 +12,7 @@
                 <div class="skw-page__skewed">
                     <div class="skw-page__content">
                         <h2 class="skw-page__heading">Hi April!</h2>
-                        <p class="skw-page__description">Seperti biasa, bukan aku kalo ga unprectable. tapi mungkin udah
+                        <p class="skw-page__description">Seperti biasa, bukan aku kalo ga unpredictable. tapi mungkin udah
                             ketebak si kalo aku bakalan bikin website lagi. Tapi tenang, kali ini lebih beda.</p>
                     </div>
                 </div>
@@ -43,9 +43,9 @@
             <div class="skw-page__half skw-page__half--right">
                 <div class="skw-page__skewed">
                     <div class="skw-page__content">
-                        <p class="skw-page__description">Happy birthday ya, wish aku yang terbaik buat kamu. Tetap semangat
-                            kaya april yang aku kenal, jaga kesehatan kamu, perbanyak istirahat juga disela-sela kesibukan
-                            kamu biar kamu ga sakit. kaya tangan beomgyu tuh, berdarah.</p>
+                        <p class="skw-page__description">Happy birthday baby ❤️ Doa aku selalu yang terbaik buat kamu.
+                            Semoga mimpi dan cita-cita kamu tercapai segera yaa, aku yakin kamu pasti bisa dapetin apa yang
+                            kamu mau. Aku selalu dukung kamu. Happy birthday ❤️</p>
                     </div>
                 </div>
             </div>
@@ -54,11 +54,9 @@
             <div class="skw-page__half skw-page__half--left">
                 <div class="skw-page__skewed">
                     <div class="skw-page__content">
-                        <p class="skw-page__description">By the way, semangat ya ngerjain skripsi-nya. aku yakin sesusah
-                            apapun, kamu pasti bisa. karna kamu hebat. Aku tau kamu itu gimana, jadi... semangat. Oiya, aku
-                            gapernah cape bilang ini ke kamu. Aku selalu ada buat kamu, kapanpun,
-                            dimanapun. kalo butuh bantuan kabarin aku ya. aku pasti bantu semaksimal mungkin. Anyway, Tuh
-                            anak anak ngasih bunga ke kamu.</p>
+                        <p class="skw-page__description">Semoga diberikan kemudahan, diberikan kekuatan, dan diberikan
+                            kesabaran dalam menghadapi masalah yang ada. Jangan lupa istirahat dan makan yaa... biar kamu ga
+                            sakit dan apa yang kamu kerjain sekarang ga ketunda</p>
                     </div>
                 </div>
             </div>
@@ -69,6 +67,71 @@
             </div>
         </div>
         <div class="skw-page skw-page-5">
+            <div class="skw-page__half skw-page__half--left">
+                <div class="skw-page__skewed">
+                    <div class="skw-page__content"></div>
+                </div>
+            </div>
+            <div class="skw-page__half skw-page__half--right">
+                <div class="skw-page__skewed">
+                    <div class="skw-page__content">
+                        <p class="skw-page__description">Oiya, semangat dan semoga lancar puasanya! Ice cream dari aku
+                            selalu nunggu buat dimakan sama kamu hehehe... tapi jangan banyak-banyak ya, gabagus juga buat
+                            kamu... ohh, kurangi takaran pedesnya, inget!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="skw-page skw-page-6">
+            <div class="skw-page__half skw-page__half--left">
+                <div class="skw-page__skewed">
+                    <div class="skw-page__content">
+                        <p class="skw-page__description">Kamu semangat ya ngerjain skripsi-nya. aku yakin sesusah
+                            apapun, kamu pasti bisa karna kamu hebat. Selama aku ada di samping kamu, aku usahakan yang
+                            terbaik buat bantu kamu. Jemput kamu, anter kamu, temenin kamu, aku selalu siap.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="skw-page__half skw-page__half--right">
+                <div class="skw-page__skewed">
+                    <div class="skw-page__content"></div>
+                </div>
+            </div>
+        </div>
+        <div class="skw-page skw-page-7">
+            <div class="skw-page__half skw-page__half--left">
+                <div class="skw-page__skewed">
+                    <div class="skw-page__content"></div>
+                </div>
+            </div>
+            <div class="skw-page__half skw-page__half--right">
+                <div class="skw-page__skewed">
+                    <div class="skw-page__content">
+                        <p class="skw-page__description">Slowly but sure... sesuah apapun itu, progress 1% sekalipun cukup,
+                            yang penting masih terus berusaha. jangan pernah berhenti yaa~ Kita belajar sama-sama buat jadi
+                            lebih baik lagi. Semangat sayang ❤️</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="skw-page skw-page-8">
+            <div class="skw-page__half skw-page__half--left">
+                <div class="skw-page__skewed">
+                    <div class="skw-page__content">
+                        <p class="skw-page__description">Okey, sekian dari aku hehehe, tunggu surprise lainnya ya... soalnya
+                            ga cuma ini aja yang aku siapin. oiya salam ya buat bapak sama ibu, kaka dan mas yuda, dan
+                            jeanne.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="skw-page__half skw-page__half--right">
+                <div class="skw-page__skewed">
+                    <div class="skw-page__content"></div>
+                </div>
+            </div>
+        </div>
+        <div class="skw-page skw-page-9">
             <div class="skw-page__half skw-page__half--left">
                 <div class="skw-page__skewed">
                     <div class="skw-page__content"></div>
@@ -93,8 +156,8 @@
             <div class="image-placement-1"></div>
             <div class="description-mobile">
                 <h1 class="mt-5">Hi April!</h1>
-                <p>Seperti biasa, bukan aku kalo ga unprectable. tapi mungkin udah ketebak si kalo aku bakalan bikin website
-                    lagi. Tapi tenang, kali ini lebih beda.</p>
+                <p>Seperti biasa, bukan aku kalo ga unpredictable. tapi mungkin udah
+                    ketebak si kalo aku bakalan bikin website lagi. Tapi tenang, kali ini lebih beda.</p>
             </div>
         </div>
 
@@ -102,7 +165,8 @@
             <div class="image-placement-2"></div>
             <div class="description-mobile">
                 <h1 class="mt-5">Apa yang beda?</h1>
-                <p>Hmmm, pokoknya beda. kamu ikutin terus aja ya alur websitenya akan membawa kamu kemana. hehehe :).</p>
+                <p>Hmmm, pokoknya beda. kamu ikutin terus aja ya alur websitenya akan
+                    membawa kamu kemana. hehehe :).</p>
             </div>
         </div>
 
@@ -110,9 +174,9 @@
             <div class="image-placement-3"></div>
             <div class="description-wrapped">
                 <div class="description-mobile">
-                    <p class="mt-5">Happy birthday ya, wish aku yang terbaik buat kamu. Tetap semangat kaya april
-                        yang aku kenal, jaga kesehatan kamu, perbanyak istirahat juga disela-sela kesibukan kamu biar kamu
-                        ga sakit. kaya tangan beomgyu tuh, berdarah.</p>
+                    <p class="mt-5">Happy birthday baby ❤️ Doa aku selalu yang terbaik buat kamu.
+                        Semoga mimpi dan cita-cita kamu tercapai segera yaa, aku yakin kamu pasti bisa dapetin apa yang
+                        kamu mau. Aku selalu dukung kamu. Happy birthday ❤️</p>
                 </div>
             </div>
         </div>
@@ -121,11 +185,9 @@
             <div class="image-placement-4"></div>
             <div class="description-wrapped">
                 <div class="description-mobile">
-                    <p class="mt-5">By the way, semangat ya ngerjain skripsi-nya. aku yakin sesusah apapun, kamu
-                        pasti bisa. karna kamu hebat. Aku tau kamu itu gimana, jadi... semangat. Oiya, aku gapernah cape
-                        bilang ini ke kamu. Aku selalu ada buat kamu, kapanpun, dimanapun.
-                        kalo butuh bantuan kabarin aku ya. aku pasti bantu semaksimal mungkin. Anyway, Tuh anak anak ngasih
-                        bunga ke kamu.</p>
+                    <p class="mt-5">Semoga diberikan kemudahan, diberikan kekuatan, dan diberikan
+                        kesabaran dalam menghadapi masalah yang ada. Jangan lupa istirahat dan makan yaa... biar kamu ga
+                        sakit dan apa yang kamu kerjain sekarang ga ketunda</p>
                 </div>
             </div>
         </div>
@@ -134,8 +196,59 @@
             <div class="image-placement-5"></div>
             <div class="description-wrapped">
                 <div class="description-mobile">
-                    <h1 class="mt-5">Last but not least for me.</h1>
                     <p>
+                        Oiya, semangat dan semoga lancar puasanya! Ice cream dari aku
+                        selalu nunggu buat dimakan sama kamu hehehe... tapi jangan banyak-banyak ya, gabagus juga buat
+                        kamu... ohh, kurangi takaran pedesnya, inget!
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="pages">
+            <div class="image-placement-6"></div>
+            <div class="description-wrapped">
+                <div class="description-mobile">
+                    <p>
+                        Kamu semangat ya ngerjain skripsi-nya. aku yakin sesusah
+                        apapun, kamu pasti bisa karna kamu hebat. Selama aku ada di samping kamu, aku usahakan yang
+                        terbaik buat bantu kamu. Jemput kamu, anter kamu, temenin kamu, aku selalu siap.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="pages">
+            <div class="image-placement-7"></div>
+            <div class="description-wrapped">
+                <div class="description-mobile">
+                    <p>
+                        Slowly but sure... sesuah apapun itu, progress 1% sekalipun cukup,
+                        yang penting masih terus berusaha. jangan pernah berhenti yaa~ Kita belajar sama-sama buat jadi
+                        lebih baik lagi. Semangat sayang ❤️
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="pages">
+            <div class="image-placement-8"></div>
+            <div class="description-wrapped">
+                <div class="description-mobile">
+                    <p>
+                        Okey, sekian dari aku hehehe, tunggu surprise lainnya ya... soalnya
+                        ga cuma ini aja yang aku siapin. oiya salam ya buat bapak sama ibu, kaka dan mas yuda, dan jeanne.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="pages">
+            <div class="image-placement-9"></div>
+            <div class="description-wrapped">
+                <div class="description-mobile">
+                    <h2 class="skw-page__heading">Last but not least from me.</h2>
+                    <p class="skw-page__description">
                         Ada banyak twit dari orang-orang buat kamu hari ini. kalo ga percaya coba aja cek
                         <a class="skw-page__link" href="{{ route('twitter.index') }}">Twitter</a>
                     </p>
